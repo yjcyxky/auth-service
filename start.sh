c98e3e5a68406adaa17c50e220b67c7c03f07b16
YJC <yjcyxky@163.com>
2022-04-09 15:28:43 +0800

    First Commit.

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# docker network create quartet-network
+docker-compose build kong
+docker-compose up -d kong-db
+docker-compose run --rm kong kong migrations bootstrap
+docker-compose run --rm kong kong migrations up
+docker-compose up -d kong
+docker-compose ps
+curl -s http://localhost:8001 | jq .plugins.available_on_server.oidc
+docker-compose up -d konga
+sleep 120
+docker-compose up -d keycloak
+docker-compose ps
